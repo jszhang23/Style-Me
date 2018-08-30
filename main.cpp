4e9d2cf291865a22bd313160028d6ae0271de9a8
Zach Bernstein <zebernst@gmail.com>
2018-08-30 11:11:13 -0400

    fix style according to following rules: - braces: egyptian - case: snake - comments: rme - variables: no prefix

--- main.cpp
+++ main.cpp
@@ -1,45 +1,101 @@
 #include <iostream>
 using namespace std;
 
-class temp {
-    double f;
+/* * * * * Temperature Class Declaration * * * * */
+class Temperature {
+private:
+    // fields
+    double fahrenheit;
 public:
-    temp() : f(32) {}
-    double getf() const {return f;}
-    void setf(double p)
-    { f = p;}
-    double getc() const {
-        return (f-32)*(5/9);        // NEED TO FIX
-    }
-    double setc(double c) { f=(c*(9/5)+32);} //BUGGY
+    // default constructor
+    // requires: none
+    // modifies: fahrenheit
+    // effects: sets fields to default values
+    Temperature();
+
+    // getters
+    // requires: none
+    // modifies: none
+    // effects: returns value of field
+    double get_fahrenheit() const;
+    double get_celsius() const;
+
+    // setters
+    // requires: new value
+    // modifies: field being set
+    // effects: sets field to new specified value
+    void set_fahrenheit(double tempF);
+    void set_celsius(double tempC);
 };
 
+/* * * * * Global Function Declarations * * * * */
+
+// requires: console prompt as string
+// modifies: cin, cout
+// effects: prompts user for integer with specified string and returns the integer from the
+//          console, after ensuring that the user did in fact provide an integer.
+int get_integer_from_console_with_prompt(const string &prompt);
+
+
 int main() {
-    int choice = 0;
-    while (!(cin >> choice))
-    {
-        cin.clear();
-        string junk;
-        getline(cin, junk);
+    int degree_system = get_integer_from_console_with_prompt("celsius (0) or fahrenheit (1): ");
+    if (degree_system < 0 || degree_system > 1) {
+        cout << "invalid degree system selection!";
+        exit(1);
     }
-    int t = 0;
-    while (!(cin >> t)) {
-        cin.clear();
-        string junk;
-        getline(cin, junk);
+    int input_temperature = get_integer_from_console_with_prompt("temperature: ");
+
+    Temperature new_temperature;
+
+    if (degree_system == 1) {
+        new_temperature.set_fahrenheit(input_temperature);
     }
-    temp mytemp;
-    if (choice == 1)
-        mytemp.setf(t);
-    else
-        mytemp.setc(t);
-    string a;
-    if (mytemp.getc() < 0)
-        a="cold";
-    else if (mytemp.getc() > 30)
-        a="hot";
-    else
-        a="beautiful";
-    cout << "Hello, " << a << " World!" << endl;
+    else if (degree_system == 0) {
+        new_temperature.set_celsius(input_temperature);
+    }
+
+    string adjective;
+    if (new_temperature.get_celsius() <= 0) {
+        adjective = "cold";
+    }
+    else if (new_temperature.get_celsius() >= 30) {
+        adjective = "hot";
+    }
+    else {
+        adjective = "beautiful";
+    }
+    cout << "Hello, " << adjective << " world!" << endl;
     return 0;
+}
+
+/* * * * * Temperature Class Implementation * * * * */
+
+Temperature::Temperature() : fahrenheit(32) {}
+
+double Temperature::get_fahrenheit() const {
+    return fahrenheit;
+}
+void Temperature::set_fahrenheit(double tempF) {
+    fahrenheit = tempF;
+}
+double Temperature::get_celsius() const {
+    return (fahrenheit - 32) * (5./9);
+}
+void Temperature::set_celsius(double tempC) {
+    fahrenheit = tempC * (9./5) + 32;
+}
+
+/* * * * * Global Function Implementations * * * * */
+int get_integer_from_console_with_prompt(const string &prompt) {
+    int input = 0;
+    cout << prompt;
+
+    // validate input
+    while (!(cin >> input)) {
+        cin.clear();
+        string junk;
+        getline(cin, junk);
+    }
+
+    return input;
 }
