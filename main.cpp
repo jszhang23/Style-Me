739c417d45eeb1c6ed146eb862579289271414b8
jweiland22 <jpatch1@gmail.com>
2018-08-30 11:11:55 -0400

    Working on updating style for this bad code!

--- main.cpp
+++ main.cpp
@@ -1,42 +1,66 @@
 #include <iostream>
 using namespace std;
 
-class temp {
-    double f;
+class Temperature {
+    double fTemperature;
+
 public:
-    temp() : f(32) {}
-    double getf() const {return f;}
-    void setf(double p)
-    { f = p;}
-    double getc() const {
-        return (f-32)*(5/9);        // NEED TO FIX
+    // Default constructor
+    // Requires: optional double input
+    // Modifies: fTemperature
+    // Effects: sets fields to default values
+    Temperature() : fTemperature(32) {
     }
-    double setc(double c) { f=(c*(9/5)+32);} //BUGGY
+
+    // Getters
+    // Requires: nothing
+    // Modifies: nothing
+    // Effects: returns temperature in Fahrenheit or Celsius
+    double getFahrenheit() const {
+        return fTemperature;
+    }
+    double getCelsius() const {
+        return (fTemperature-32)/1.8;
+    }
+
+    // Setters
+    // Requires: input temperature (double)
+    // Modifies: fTemperature
+    // Effects: sets the fields
+    void setFahrenheit(double pTemp) {
+        fTemperature = pTemp;
+    }
+    void setCelsius(double pTemp) {
+        fTemperature=(pTemp*(1.8)+32);
+    }
 };
 
 int main() {
     int choice = 0;
-    while (!(cin >> choice))
-    {
+    cout << "Enter 1 for Fahrenheit or 2 for Celsius" << endl;
+    while (!(cin >> choice)) {
         cin.clear();
         string junk;
         getline(cin, junk);
+        cout << "Enter 1 or 2" << endl;
     }
     int t = 0;
+    cout << "Enter a temperature" << endl;
     while (!(cin >> t)) {
         cin.clear();
         string junk;
         getline(cin, junk);
+        cout << "Temperature must be a number" << endl;
     }
-    temp mytemp;
+    Temperature myTemp;
     if (choice == 1)
-        mytemp.setf(t);
+        myTemp.setFahrenheit(t);
     else
-        mytemp.setc(t);
+        myTemp.setCelsius(t);
     string a;
-    if (mytemp.getc() < 0)
+    if (myTemp.getCelsius() <= 0)
         a="cold";
-    else if (mytemp.getc() > 30)
+    else if (myTemp.getCelsius() > 30)
         a="hot";
     else
         a="beautiful";
