cd53c18db07415be73a37ca7d2b586f1865b0d26
Ethan Nerney <enerney@uvm.edu>
2018-09-04 14:28:36 -0400

    style changes

--- main.cpp
+++ main.cpp
@@ -1,45 +1,70 @@
 #include <iostream>
 using namespace std;
 
-class temp {
-    double f;
+class Temperature {
+private:
+    double fFahrenheit;
+
 public:
-    temp() : f(32) {}
-    double getf() const {return f;}
-    void setf(double p)
-    { f = p;}
-    double getc() const {
-        return (f-32)*(5/9);        // NEED TO FIX
+    Temperature() : fFahrenheit(0) {}
+
+
+    double getFahrenheit() const {
+        return fFahrenheit;
     }
-    double setc(double c) { f=(c*(9/5)+32);} //BUGGY
+
+
+    void setFahrenheit(double aFahrenheit) {
+        fFahrenheit = aFahrenheit;
+    }
+
+
+    double getCelsius() const {
+        return (fFahrenheit - 32) * (5 / 9.0);        // NEED TO FIX
+    }
+
+
+    void setCelsius(double aCelsius) {
+        setFahrenheit(aCelsius * (9/5.0) + 32);
+    } //BUGGY
 };
 
 int main() {
+    cout << "Enter 0 to set Celsius, 1 to set Fahrenheit" << endl;
     int choice = 0;
-    while (!(cin >> choice))
-    {
+    while (!(cin >> choice)) {
         cin.clear();
         string junk;
         getline(cin, junk);
     }
+
+    cout << "Enter Temperature" << endl;
     int t = 0;
     while (!(cin >> t)) {
         cin.clear();
         string junk;
         getline(cin, junk);
     }
-    temp mytemp;
-    if (choice == 1)
-        mytemp.setf(t);
-    else
-        mytemp.setc(t);
+
+    Temperature myTemp;
+
+    if (choice == 1) {
+        myTemp.setFahrenheit(t);
+    } else {
+        myTemp.setCelsius(t);
+    }
+
     string a;
-    if (mytemp.getc() < 0)
-        a="cold";
-    else if (mytemp.getc() > 30)
-        a="hot";
-    else
-        a="beautiful";
+    if (myTemp.getCelsius() < 0) {
+        a = "cold";
+    } else if (myTemp.getCelsius() > 30) {
+        a = "hot";
+    } else {
+        a = "beautiful";
+    }
+
+    cout << "Celsius " << myTemp.getCelsius() << endl;
+    cout << "Fahrenheit " << myTemp.getFahrenheit() << endl;
     cout << "Hello, " << a << " World!" << endl;
     return 0;
 }
